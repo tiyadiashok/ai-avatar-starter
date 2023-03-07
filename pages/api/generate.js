b3ab8ab1b828090baf057b557c1212a65eccad0e
tiyadiashok <tiyadiashok1@gmail.com>
2023-03-07 23:37:58 +0530

    Initial Commit

---
+++ pages/api/generate.js
@@ -0,0 +1,40 @@
+const bufferToBase64 = (buffer) => {
+    let arr = new Uint8Array(buffer);
+    const base64 = btoa(
+        arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+    )
+    return `data:image/png;base64,${base64}`;
+};
+
+const generateAction = async (req, res) => {
+    console.log('Received request..');
+    const input = JSON.parse(req.body).input;
+    const response = await fetch(
+        `https://api-inference.huggingface.co/models/`.concat(process.env.HF_USER_MODEL),
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+            'Content-Type': 'application/json',
+            'x-use-cache': 'false'
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            inputs: input,
+          }),
+        }
+    );
+
+    if (response.ok) {
+        const buffer = await response.arrayBuffer();
+        const base64 = bufferToBase64(buffer);
+        res.status(200).json({ image: base64 });
+    } else if (response.status === 503) {
+        const json = await response.json();
+        res.status(503).json(json);
+    } else {
+        const json = await response.json();
+        res.status(response.status).json({ error: response.statusText });
+    }
+}
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,97 @@
+
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState(''); 
+  const [retry, setRetry] = useState(0);
+  // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState('');
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+        }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
+  const generateAction = async () => {
+    console.log('Generating...');
+
+    setIsGenerating(true);
+
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+    // Add the fetch request
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    // If model still loading, drop that retry time
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
+      return;
+    }
+
+    setFinalPrompt(input);
+    // Remove content from input box
+    setInput('');
+    setImg(data.image);
+    setIsGenerating(false);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +100,41 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Silly picture generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "tiyadiashok" in the prompt</h2>
+          </div>
+          {/* Add prompt container here */}
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange}/>
+            <div className="prompt-buttons">
+              {/* Tweak classNames to change classes */}
+              <a
+                className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+                }
+                onClick={generateAction}
+              >
+                {/* Tweak to show a loading indicator */}
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+          </div>
           </div>
         </div>
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={input} />
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
